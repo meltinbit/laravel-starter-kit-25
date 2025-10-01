05af03c22cc3517290f37d17da00c866aedca141
Meltin Bit <fabio@meltinbit.com>
2025-10-01 12:38:14 +0200

    Create dev.sh

---
+++ dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if .env file exists
+if [ ! -f .env ]; then
+    echo "Creating .env file..."
+    cp .env.example .env
+    php artisan key:generate
+fi
+
+# Install/Update PHP dependencies
+echo "Installing/Updating PHP dependencies..."
+composer install
+
+# Install/Update Node.js dependencies
+echo "Installing/Updating Node.js dependencies..."
+npm install
+
+# Start development servers
+echo "Starting development servers..."
+composer run dev
